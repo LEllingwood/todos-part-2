5ec721d91e254fbaf2d1730db224716f62cc0348
Lea Ellingwood <leaellingwood@hotmail.com>
2018-11-15 16:08:51 -0500

    b

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,16 +1,8 @@
 
 import React, { Component } from "react";
 import "./App.css";
-// We added line 7  so we can use todoList as a variable for todays assessment (that makes sense)
 import todoList from "./todos.json";
 
-// I think the issue would be that the TodoList is meant to be a componenet that contains the todo items. If we make it the whole html page the TodoList component wouldn't be able to be used again. It's really just a data structure.
-// I Actually you're right only two and yea the app component is like the webpage the tictactoe didn't have a whole layout structure
-// app contains the two images I just slacked. I see.  Ok, that's helpful..  sorry to get sidetracked. All good so have you completed yesterdays assessmnt?
-
-// No, not at all.  I had a town board meeting (I'm a volunteer attorney for the town) until 10:30 last night in Crawfordsville, so I didn't get home until about midnight.  that's approximately 8 hours past my bedtime...lol omg
-
-// do you mind if I comment in questions through the code below?{sure}
 class TodoItem extends Component {
   render() {
     return (
@@ -24,8 +16,6 @@ class TodoItem extends Component {
           />
           <label>{this.props.title}</label>
           <button className="destroy" onClick={this.props.handleDeleteTodo} />
-          {/* what is "destroy" about? */}
-          {/* we'll connect it to the delete function OK*/}
         </div>
       </li>
     );
@@ -35,9 +25,7 @@ class TodoItem extends Component {
 class TodoList extends Component {
   render() {
     return <ul className="todo-list">{this.props.children}</ul>;
-    // what's going on with this line?
     // This it the ul tag that acts as a list container for all the TodoItems that are added.
-    // What's weird to me is that below we don't have to define the prop children=line 61" Like do you see how we had to define the other two props {title and completed} we made above? title={todo.title} completed={todo.completed}.  That is weird.  Did Taylor explain why? Let me see if we can live share with him too. its just a link I share. OK
   }
 }
 class App extends Component {
@@ -100,18 +88,19 @@ class App extends Component {
 
   // add new todos.
   addNewTodo = event => {
-    if (event.keyCode === 13 /* the number for <ENTER> key */) {
-      const newTodos = this.state.todos.slice(0);
-      // newTodo = <todoItem>??</todoItem>
-      // get the title from event.target.value
-      // id for the new todo. use random number generator within a range to assign to the new todo. to make sure that you dont have two lists with the same to do.
-      const newTodo = {
-        id: 0 /* Math.floor(Math.random()) generate random number here */
+    if (event.keyCode === 13) {
+      let newTodos = this.state.todos.slice(0);
+      let newTodo = {
+        "userId": 1,
+        "id": Math.floor(Math.random() * 100) + 1,
+        "title": event.target.value,
+        "completed": false
       };
       newTodos.push(newTodo);
       this.setState({
         todos: newTodos
       });
+      console.log(this.state)
     }
   };
 
@@ -124,6 +113,7 @@ class App extends Component {
             className="new-todo"
             placeholder="What needs to be done?"
             autoFocus
+            onKeyDown={this.addNewTodo}
           />
         </header>
         <section className="main">
